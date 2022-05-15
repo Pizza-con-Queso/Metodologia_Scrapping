11d59df067968592e17fa74bffd5fcd687bc5193
elChosenOne <tomas.fuentes.palma@gmail.com>
2022-05-14 20:32:54 -0400

    Separado el detector de genero en una funcion. Añadido detector de url. Añadido buscador de paises *no crea tabla

--- PruebaScraping.py
+++ PruebaScraping.py
@@ -1,17 +1,56 @@
 import requests
+import math
 from bs4 import BeautifulSoup
+
+def DetectarGenero(sop):
+    ini = sop.find("The fields of study he is best known for:")
+    fin = sop.find("His most cited work include:")
+    print(sop.find("his", ini, fin))
+    print(sop.find("her", ini, fin))
+
+def DetectarLinks(sop):
+    ini = 0
+    fin = 0
+    while ini != -1:
+        ini = sop.find("href=\"http", ini)
+        fin = sop.find("\"", ini+6)
+        if ini != -1:
+            print("(",ini,",",fin,")")
+            print(sop[ini+6:fin])
+            ini = fin
+
+def TablaPaises(sop):
+    ini = sop.find("All countries")
+    fin = 0
+    final = sop.find("/select", ini)
+    Lista = []
+    LF = []
+    while fin != -1 and (ini != -1 and ini < final):
+        ini = sop.find("\">", ini)
+        fin = sop.find("\n</option>", ini)
+        if fin != -1:
+            Lista.append(str(sop[ini+3:fin]))
+        ini = ini+1
+        
+    for i in range(len(Lista)):
+        T = []
+        ini = Lista[i].find("(")
+        fin = Lista[i].find(")")
+        print(Lista[i][0:ini])
+        print(Lista[i][ini+1:fin])
+        T.append( Lista[i][0:ini])
+        T.append(int(Lista[i][ini+1:fin]))
+        LF += T
+    print(LF)
 
 
 head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}
 
-page = requests.get('https://research.com/u/anil-k-jain/', headers=head)
+page = requests.get('https://research.com/scientists-rankings/computer-science', headers=head)
 print(page)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 soup = str(soup)
 
-ini = soup.find("The fields of study he is best known for:")
-fin = soup.find("His most cited work include:")
-
-print(soup.find("his", ini, fin))
-print(soup.find("her", ini, fin))
+#DetectarLinks(soup)
+TablaPaises(soup)
