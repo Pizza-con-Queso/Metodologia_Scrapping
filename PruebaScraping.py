a239ac2622aa1d004efc8bc102930158c64a6fef
elChosenOne <tomas.fuentes.palma@gmail.com>
2022-05-25 20:35:03 -0400

    Agregado deteccion de generos por paises

--- PruebaScraping.py
+++ PruebaScraping.py
@@ -1,47 +1,90 @@
 import requests
 import math
+
+import time
+
 from bs4 import BeautifulSoup
 
 def DetectarGenero(sop):
-    ini = sop.find("The fields of study he is best known for:")
-    fin = sop.find("His most cited work include:")
-    print(sop.find("his", ini, fin))
-    print(sop.find("her", ini, fin))
+    ini = sop.find("What is")
+    fin = sop.find("best known for?")
+    if (sop.find("she", ini, fin) > 0):
+        return 0
+    elif (sop.find("he", ini, fin) > 0):
+        return 1
+    else:
+        return 2
+        
+#retornar resultado, crear funcion aplicadora
 
-def DetectarLinks(sop):
-    ini = 0
-    fin = 0
-    while ini != -1:
-        ini = sop.find("href=\"http", ini)
-        fin = sop.find("\"", ini+6)
-        if ini != -1:
-            print("(",ini,",",fin,")")
-            print(sop[ini+6:fin])
-            ini = fin
+def DetectarGeneroPorPais(head, TabCien):
+    Res = [0, 0, 0]
+    
+    for j in range(len(TabCien)):
+        page = requests.get(TabCien[j], headers=head)
+        soap = BeautifulSoup(page.text, 'html.parser')
+        soap = str(soap)
+        Res[DetectarGenero(soap)] += 1
+    return Res
+
+def DetectarGeneroPais(head):
+    link = 'https://research.com/scientists-rankings/computer-science'
+    page = requests.get(link, headers=head)
+    soap = BeautifulSoup(page.text, 'html.parser')
+    soap = str(soap)
+    
+    TabPai = TablaPaises(soap)
+    print("Listado paises:")
+    for i in range(len(TabPai)):
+        print('\t',i+1,'-', TabPai[i][1])
+        
+    Pais = input('Ingrese el numero del pais a buscar: ')
+    Pais = int(Pais)
+    print(TabPai[Pais-1][1])
+
+    TabCien = []
+    for i in range(TabPai[Pais-1][3]):
+        page = requests.get(link+'/'+TabPai[Pais-1][0]+'?page='+str(i), headers=head)
+        soap = BeautifulSoup(page.text, 'html.parser')
+        soap = str(soap)
+        TabCien += DetectarLinksCientificos(soap)
+            
+    Lim = input('Ingrese el limite de resultados(0 para todos): ')
+    Lim = int(Lim)
+    if (Lim != 0):
+        TabCien = TabCien[0:Lim]
+    Res = DetectarGeneroPorPais(head, TabCien)
+    print("Mujeres = ", Res[0], "\nHombres = ", Res[1], "\nNo detectado = ", Res[2])
 
 def TablaPaises(sop):
     ini = sop.find("All countries")
     fin = 0
     final = sop.find("/select", ini)
     Lista = []
+    L2 = []
     LF = []
     while fin != -1 and (ini != -1 and ini < final):
         ini = sop.find("\">", ini)
         fin = sop.find("\n</option>", ini)
+        L2.append(sop[ini-2: ini])
         if fin != -1:
             Lista.append(str(sop[ini+3:fin]))
         ini = ini+1
-        
     for i in range(len(Lista)):
         T = []
         ini = Lista[i].find("(")
         fin = Lista[i].find(")")
-        print(Lista[i][0:ini])
-        print(Lista[i][ini+1:fin])
-        T.append( Lista[i][0:ini])
+        T.append(L2[i])
+        T.append( Lista[i][0:ini-1])
         T.append(int(Lista[i][ini+1:fin]))
+        T.append(math.ceil(int(Lista[i][ini+1:fin])/100))
         LF.append(T)
-    print(LF)
+    return LF
+#Retorna tabla de paises [[a, p, c, f], ...]
+#   a = acronimo pais:          string
+#   p = nombre pais:            string
+#   c = cantidad cientificos:   int
+#   f = cantidad de paginas:    int
     
 def DetectarLinksCientificos(sop):
     ini = 0
@@ -65,8 +108,10 @@ print(page)
 soup = BeautifulSoup(page.text, 'html.parser')
 soup = str(soup)
 
-#DetectarLinks(soup)
-TablaPaises(soup)
+DetectarGeneroPais(head)
+#print(DetectarGeneroPorPais(head))
+#print(DetectarLinksCientificos(soup))
+#TablaPaises(soup)
 
 def main():
     head_user = input("Ingrese su header:  ")
