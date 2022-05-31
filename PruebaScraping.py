29f96ed178ce814dd17bc2576479eaefbbf7237d
elChosenOne <tomas.fuentes.palma@gmail.com>
2022-05-30 21:07:08 -0400

    error

--- PruebaScraping.py
+++ PruebaScraping.py
@@ -95,7 +95,99 @@ def BuscarSeccion(sop, text):
     return -1
 
 def AgregarSubdiciplina(LT, N):
-    for i in LT:
+    for i in LT:ountries")
+    fin = 0
+    final = sop.find("/select", ini)
+    Lista = []
+    L2 = []
+    LF = []
+    while fin != -1 and (ini != -1 and ini < final):
+        ini = sop.find("\">", ini)
+        fin = sop.find("\n</option>", ini)
+        L2.append(sop[ini-2: ini])
+        if fin != -1:
+            Lista.append(str(sop[ini+3:fin]))
+        ini = ini+1
+    for i in range(len(Lista)):
+        T = []
+        ini = Lista[i].find("(")
+        fin = Lista[i].find(")")
+        T.append(L2[i])
+        T.append(Lista[i][0:ini-1])
+        T.append(int(Lista[i][ini+1:fin]))
+        T.append(math.ceil(int(Lista[i][ini+1:fin])/100))
+        LF.append(T)
+    return LF
+#Retorna tabla de paises [[a, p, c, f], ...]
+#   a = acronimo pais:          string
+#   p = nombre pais:            string
+#   c = cantidad cientificos:   int
+#   f = cantidad de paginas:    int
+    
+def DetectarLinksCientificos(sop):
+    ini = 0
+    fin = 0
+    T = []
+    while ini != -1:
+        ini = sop.find("href=\"https://research.com/u/", ini)
+        fin = sop.find("\"", ini+6)
+        if ini != -1:
+            T.append(sop[ini+6:fin]+"/") 
+            ini = fin
+    return T
+
+def main():
+    head_user = input("Ingrese su header:  ")
+    
+    if head_user == 't':
+        head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}
+    elif head_user == 'v':
+        head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'}
+    else:
+        head = {'User-Agent': head_user}
+    
+    page = requests.get('https://research.com/scientists-rankings/computer-science', headers=head)
+
+    if verificacion_HeadUser(page.status_code) == 1:
+        menu(head)
+    else:
+        print("El head User no es correcto. Intente nuevamente")
+
+def verificacion_HeadUser(A):
+    if A >= 200 and A < 300:
+        return 1
+    else:
+        return 0
+
+def menu(head):
+    opcion = 1
+    while(opcion > 0 and opcion <= 5):
+        intrucciones()
+        opcion = int(input("\n\nIngrese la opción que desea generar:\t"))
+        if opcion == 1:
+            DetectarPorPais(head, DetectarGeneroPorPais, PrintGeneroPais)
+        elif opcion == 2:
+            print("Hola")
+        elif opcion == 3:
+            print("Hola")
+        elif opcion == 4:
+            DetectarPorPais(head, DetectarCantidadArticulosPorPais, PrintArticulosPais)
+        elif opcion == 5:
+            print("Hola")
+        else:
+            print("Saliendo")
+        
+def intrucciones():
+    print("\n\n")
+    print("1) Identificar sexo de científicos de un pais")
+    print("2) Cantidad de Instituciones de investigación por país")
+    print("3) Coautores")
+    print("4) Sub-diciplinas con más artículos publicados")
+    print("5) Cantidad de Articulos citados por sub-diciplina")
+    print("Salir (0)")
+
+
+main()
         if i[0] == N[0]:
             i[1] += N[2]
             return
